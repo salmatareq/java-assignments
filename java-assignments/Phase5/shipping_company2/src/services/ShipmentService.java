d2771217e15ac5e5ae746c88c384e04e25eaa963
salma tarek <salmatarekamer@gmail.com>
2026-04-21 16:56:44 +0200

    add states handling

--- java-assignments/Phase5/shipping_company2/src/services/ShipmentService.java
+++ java-assignments/Phase5/shipping_company2/src/services/ShipmentService.java
@@ -4,24 +4,30 @@ import factory.PackagingFactory;
 import factory.ShippingFactory;
 import interfaces.IShipping;
 import interfaces.Ipackaging;
+import interfaces.State;
 import model.Product;
 
 public class ShipmentService {
-    public void processShipment(Product product,boolean isExpress) {
+    public void processShipment(Product product, boolean isExpress) {
 
 
         Ipackaging packaging =
                 PackagingFactory.create(product.getWeight());
 
 
-       product.setPackagingType(packaging);
+        product.setPackagingType(packaging);
 
 
         IShipping shipping =
-                ShippingFactory.create( isExpress );
+                ShippingFactory.create(isExpress);
 
         product.setShipping(shipping);
     }
+    public void moveToNextState(Product product) {
+        product.nextState();
+    }
 
-
+    public State getCurrentState(Product product) {
+       return product.getCurrentState();
+    }
 }
